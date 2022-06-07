2c897154325d21ed83d66e6c101bc1399dd585fd
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2022-06-07 12:19:54 -0600

    building the news boxes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @news = JSON.parse(@response)
   end
 
   def prices
   end
+  
 end
