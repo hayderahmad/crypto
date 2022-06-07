086de24d0d38d05b449d9be163717bdf74031ef3
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2022-06-07 15:07:04 -0600

    last video

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,9 +7,23 @@ class HomeController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,ADA,XLM,MIOTA,USDT,TRX&tsyms=USD'
+    @prices_uri = URI(@prices_url)
+    @prices_response = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_response)
   end
 
   def prices
+    require 'net/http'
+    require 'json'
+    @symbol = params[:sym]
+    if @symbol
+      @symbol = @symbol.upcase
+      @qoute_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+ @symbol +'&tsyms=USD'
+      @qoute_uri = URI(@qoute_url)
+      @qoute_response = Net::HTTP.get(@qoute_uri)
+      @qoute = JSON.parse(@qoute_response)
+    end
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   root 'home#index'
   get 'home/prices'
+  post "/home/prices" => 'home/prices'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
