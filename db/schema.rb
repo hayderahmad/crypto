a37fa65378a4fe629de08d53845b2bca48ffb05b
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2023-02-07 07:39:53 -0700

    change database to pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_16_201233) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -25,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_16_201233) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "like", default: 0
@@ -36,7 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_16_201233) do
   create_table "notification_settings", force: :cascade do |t|
     t.string "setting_config_type"
     t.json "setting_config_params"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "by_email", default: false
