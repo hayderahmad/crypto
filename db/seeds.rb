b2f0d796198bf7ae623513f561fce0aeb65a3385
hayderahmad <Hayderahamd@yahoo.com>
2023-10-22 19:22:52 -0600

    Add faker to seed fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+    Article.create(
+        title: Faker::Lorem.sentence(word_count: 3),
+        body: Faker::Lorem.paragraph(sentence_count: 2),
+        imageurl: Faker::Internet.url,
+        source: Faker::Lorem.word,
+        article_url: Faker::Internet.url,
+        article_id: Faker::Number.number(digits: 10),
+        created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+        updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+    )
+    User.create(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: "password",
+        phone: Faker::PhoneNumber.cell_phone,
+        admin: false,
+        created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+        updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+    )
+    Comment.create(
+        commenter: Faker::Name.name,
+        body: Faker::Lorem.paragraph(sentence_count: 2),
+        article_id: Article.last.id,
+        like: 1,
+        dislike: 0,
+        created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+        updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+    )
+    end
